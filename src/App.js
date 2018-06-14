9322913f72d7d67db9f06004aedefec8d2dc71d0
Maxime Burlat <maximeburlat@gmail.com>
2018-06-13 21:37:04 -0500

    basic stuff

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      view: "view-articles"
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Economist Scraper</h1>
+          <SwitchButton currentView={this.state.view} />
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
